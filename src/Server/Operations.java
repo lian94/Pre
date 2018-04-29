53c66c688a3d3e04805ab4dcc9f30d124844b23c
lian <lguo3@student.unimelb.edu.au>
2018-04-29 17:30:12 +1000

    add multi thread

--- src/Server/Operations.java
+++ src/Server/Operations.java
@@ -47,4 +47,35 @@ public class Operations {
         }
         out.writeUTF(reply.toString());
     }
+
+    public static void get(JSONObject request, DataOutputStream out, documentList docList)
+            throws IOException{
+        JSONObject reply = new JSONObject();
+        HashMap<Boolean, String> response = new HashMap<Boolean, String>();
+        if(request.has("document")){
+            try{
+                JSONObject docInfo = request.getJSONObject("document");
+                int id = docInfo.getInt("id");
+                String msg = docInfo.getString("message");
+                Document doc = new Document(id, msg);
+                response = Function.get(doc, docList);
+                System.out.println("receive doc: " + id + msg);
+                if(response.containsKey(true)){
+                    reply.put("response", "success");
+                }
+                else{
+                    reply.put("response", "error");
+                    reply.put("errorMessage", response.get(false));
+                }
+            }catch(JsonSyntaxException j){
+                reply.put("response", "error");
+                reply.put("errorMessage", "missing document");
+            }
+
+        }else{
+            reply.put("response", "error");
+            reply.put("errorMessage", "missing document");
+        }
+        out.writeUTF(reply.toString());
+    }
 }
--- src/Server/myServer.java
+++ src/Server/myServer.java
@@ -1,22 +1,38 @@
 package Server;
 
 import Resource.documentList;
+import com.google.gson.JsonParseException;
+import com.google.gson.stream.JsonReader;
 import org.apache.commons.cli.*;
+import org.json.JSONObject;
 
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class myServer {
+import org.json.simple.parser.ParseException;
+
+public class myServer extends Thread{
 
     // initial parameters
+    private ServerSocket server;
     private String hostname = "localhost";
     private int port  = 20006;
     private int TTL = 30;
     private Resource.documentList docList;
-
+    private Socket socket = null;
+    private enum Operation {POST, GET, CLEAR;}
+    public myServer(String[] args){
+        try {
+            initialize(args);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args)throws Exception{
-        myServer server = new myServer();
-        server.initialize(args);
+        myServer server = new myServer(args);
+        server.start();
     }
 
     public  void initialize(String[] args)throws Exception {
@@ -38,21 +54,122 @@ public class myServer {
             setTTL(Integer.parseInt(cmd.getOptionValue("TTL")));
         }
 
-        ServerSocket server = new ServerSocket(getPort());
+        server = new ServerSocket(getPort());
+
+
+//        Socket client = server.accept();
+//        System.out.println("connection succeed !");
+//        if (client.isConnected()) {
+//            Server s = new Server(client, docList, getHostname(), getPort(), getTTL());
+//            s.run();
+//        } else {
+//            System.out.println("exit");
+//            client.close();
+//        }
+//        //server.close();
+
+    }
+    public void run(){
         System.out.println("waiting");
+        while (true) {
+            try {
+                //wait for connection
+                socket = server.accept();
+                System.out.println("New connection established!");
+                Thread newthread = new Thread(() -> {
+                    try {
+                        handler(socket);
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                });
+                newthread.start();
 
-        Socket client = server.accept();
-        System.out.println("connection succeed !");
-        if (client.isConnected()) {
-            System.out.println("client is connected");
-            Server s = new Server(client, docList, getHostname(), getPort(), getTTL());
-            s.run();
-        } else {
-            System.out.println("exit");
-            client.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
         }
-        //server.close();
+    }
 
+    public void handler(Socket client) throws IOException, ParseException{
+        try {
+            if (!client.isClosed()) {
+
+                DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                DataInputStream buf = new DataInputStream(client.getInputStream());
+                JsonReader request = new JsonReader(new InputStreamReader(client.getInputStream()));
+
+                try {
+                    String str = buf.readUTF();
+                    parseJson(str, out);
+                } catch (IOException i) {
+                    // TODO: handle exception
+                } finally {
+                    out.close();
+                    client.close();
+                }
+            }
+        }catch(Exception e){
+            try {
+                client.close();
+            } catch (IOException e1) {
+
+            }
+        }
+
+    }
+
+    private void parseJson(String message, DataOutputStream out){
+        JSONObject response = new JSONObject();
+        try{
+            JSONObject root = new JSONObject(message);
+
+            if(root.has("command")){
+                String commandName = root.get("command").toString();
+                Operation operation = Operation.valueOf(commandName);
+                //check command and call corresponding method
+                switch(operation){
+                    case POST:{
+                        //check resource field exists
+                        Operations.post(root, out, docList);
+                        break;
+                    }
+                    default:{
+                        response.put("response", "error");
+                        response.put("errorMessage", "invalid Command");
+                        out.writeUTF(response.toString());
+                        break;
+                    }
+                }
+            }else{
+                response.put("response", "error");
+                response.put("errorMessage", "missing or incorrect type for command");
+                try {
+                    out.writeUTF(response.toString());
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                }
+            }
+        }catch (JsonParseException e) {
+            // TODO: handle exception
+            response.put("response", "error");
+            response.put("errorMessage", "missing or incorrect type for command");
+            try {
+                out.writeUTF(response.toString());
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+            }
+        }catch (Exception e){
+            response.put("response", "error");
+            response.put("errorMessage", "missing or incorrect type for command");
+            try {
+                out.writeUTF(response.toString());
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+            }
+        }
     }
 
     public String getHostname() {
